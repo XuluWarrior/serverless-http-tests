07170c6b700bc266044fd09dfb1bd95d16823539
XuluWarrior <github@metaliving.plus.com>
2018-04-20 17:27:42 +0100

    Meaningful base path (serverless-http-tests) Use base path in server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.use('/static', express.static('public'));
+app.get('/serverless-http-tests', (req, res) => res.send('Hello World!'));
+app.use('/serverless-http-tests/static', express.static('public'));
 
 module.exports.handler = serverless(app);
 
