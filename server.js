6aec91977444e93a90667ef99bf0bcf31000835b
XuluWarrior <github@metaliving.plus.com>
2018-04-23 12:42:24 +0100

    Display url params

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
 
-app.get('/serverless-http-tests', (req, res) => res.send('Hello World!'));
+app.get('/serverless-http-tests', (req, res) => {
+    const {baseUrl, originalUrl, url} = req;
+    res.send({baseUrl, originalUrl, url});
+});
 app.use('/serverless-http-tests/static', express.static('public'));
 
 module.exports.handler = serverless(app);
